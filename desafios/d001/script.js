e8b1a53fa1a78dc6813bbd251eb87688c04e98e4
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-01-19 12:51:32 -0300

    d001 v2.0
    adicionei minutos e o horário da madrugada, por conta própria

--- desafios/d001/script.js
+++ desafios/d001/script.js
@@ -1,7 +1,13 @@
 var agora = new Date()
 var hora = agora.getHours()
+var minutos = agora.getMinutes()
 var caixa_hora = document.getElementById('hora')
-if (hora < 12){
+var caixa_minutos = document.getElementById('minutos')
+
+if (hora < 6) {
+    document.body.style.background = 'black'
+    document.getElementById('imagem_horario').src = 'imagens/madrugada.png'
+}else if (hora < 12){
     document.body.style.background = '#CFBE8F'
     document.getElementById('imagem_horario').src = 'imagens/manha.png'
 } else if (hora < 18){
@@ -11,4 +17,6 @@ if (hora < 12){
     document.body.style.background = '#48494A'
     document.getElementById('imagem_horario').src = 'imagens/noite.png'
 }
+
 caixa_hora.innerText = hora
+caixa_minutos.innerText = minutos
