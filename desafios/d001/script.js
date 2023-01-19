3775bcf9f5f8f0ed1d739674f14d41594ba9ff89
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-01-19 12:04:43 -0300

    d001
    desafio 001 completo, feito do meu jeito baseado no exemplar do Gustavo Guanabara do Cursoemvídeo

---
+++ desafios/d001/script.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+var caixa_hora = document.getElementById('hora')
+if (hora < 12){
+    document.body.style.background = '#CFBE8F'
+    document.getElementById('imagem_horario').src = 'imagens/manha.png'
+} else if (hora < 18){
+    document.body.style.background = '#AC7B66'
+    document.getElementById('imagem_horario').src = 'imagens/tarde.png'
+} else {
+    document.body.style.background = '#48494A'
+    document.getElementById('imagem_horario').src = 'imagens/noite.png'
+}
+caixa_hora.innerText = hora
