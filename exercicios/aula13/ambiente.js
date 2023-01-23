6918cf52f6b653cb273512e3de03bec5636812ea
JoaoPedroRibeiroLima <111246249+JoaoPedroRibeiroLima@users.noreply.github.com>
2023-01-23 00:20:55 -0300

    ambiente.js

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log('hello, world!!!')
+    c++
+} while (c <= 6)
+
+
+/* var c = 1
+while (c <= 6) {
+    console.log('hello, world!!!')
+    c++
+} */
+
+
+/* console.log('hello, world!!!')
+console.log('hello, world!!!')
+console.log('hello, world!!!')
+console.log('hello, world!!!')
+console.log('hello, world!!!')
+console.log('hello, world!!!') */
